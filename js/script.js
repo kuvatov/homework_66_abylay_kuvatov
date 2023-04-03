cc5f2f54fe9508b58ecb60cbe1469a03c8813299
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-03 17:10:05 +0600

    Выполнил задание 2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ console.log(document.getElementById("string-2").textContent);
 console.log(document.getElementById("string-6").textContent);
 console.log(document.getElementById("string-4").textContent);
 console.log(document.getElementById("string-1").textContent);
+
+// task2
+const elements = document.getElementsByClassName("element");
+for (let i = 0; i < elements.length; i++) {
+    if (i < 3) {
+    elements[i].style.color = "red";
+    } else {
+    elements[i].style.color = "green";
+    }
+}
