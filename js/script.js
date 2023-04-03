ee67fb9fea8840dd9e30135de53717e33c96caa9
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-03 17:08:10 +0600

    Создал файлы index.html и script.css, а также добавил файл .gitignore

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// task1
+console.log(document.getElementById("string-3").textContent);
+console.log(document.getElementById("string-5").textContent);
+console.log(document.getElementById("string-2").textContent);
+console.log(document.getElementById("string-6").textContent);
+console.log(document.getElementById("string-4").textContent);
+console.log(document.getElementById("string-1").textContent);
